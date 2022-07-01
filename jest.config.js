045703b793393447b9e6e3b6e27fb7aa345db9f7
Vincent Le Badezet <39405530+vincentljn@users.noreply.github.com>
2022-07-01 08:41:44 +0200

    Upgrade dependencies to resolve vulnerabilities, #24 (#25)
    * chore(deps): Upgrade dependencies to resolve a maximum of vulnerabilities, #24
    
    * chore(deps): Restore request dependency, #24
    
    * test: Fix jest-extended config, #24

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'ts', 'json', 'node'],
   reporters: ['default', 'jest-junit'],
   roots: [`<rootDir>/${process.env.ENV_TEST === 'int' ? 'tests/integration' : srcDir}`],
-  setupFilesAfterEnv: ['jest-extended'],
+  setupFilesAfterEnv: ['jest-extended/all'],
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.(js|ts)', '**/?*.(spec|test).(js|ts)'],
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
